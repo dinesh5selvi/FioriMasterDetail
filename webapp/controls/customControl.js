ca3fc5453467b43e10b16f428f2323c6e593a947
vishalkwatra <vishal.kwatra88@gmail.com>
2019-12-10 14:51:09 +0000

    [Feature]: Custom control with properties and get Properties usage in renderer

--- webapp/controls/customControl.js
+++ webapp/controls/customControl.js
@@ -1,11 +1,17 @@
 sap.ui.define([], function () {
 
-	sap.ui.core.Control.extend("oil.ds.controls.customControl", {
+	return sap.ui.core.Control.extend("oil.ds.controls.customControl", {
 		//metadata
-		metadata: {},
+		metadata: {
+			properties: {
+				"arya": "",
+				"color": ""
+			}
+		},
 		init: function () {},
 		renderer: function (oRm, oControl) {
-			oRm.write("<h1>Custom control printing heading</h1>");
+			// oRm.write("<h1>Custom control printing heading</h1>");
+			oRm.write('<h1 style="color: ' + oControl.getColor() + '" >' + "Custom control printing in color" + "</h1>");
 		}
 	});
 
